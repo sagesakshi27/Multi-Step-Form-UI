1882056b644451ea997dba3859dcbcaa070f1a2b
Sakshi Chavan <sakshichavan4151@gmail.com>
2026-04-22 22:30:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,51 @@
+const steps = Array.from(document.querySelectorAll("form .step"));
+const nextBtn = document.querySelectorAll(".next-btn");
+const prevBtn = document.querySelectorAll(".previous-btn");
+const form = document.querySelector("form");
 
+nextBtn.forEach(button => {
+    button.addEventListener("click", () => {
+        changeStep("next");
+    });
+});
+
+prevBtn.forEach(button => {
+    button.addEventListener("click", () => {
+        changeStep("prev");
+    });
+});
+
+form.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+    const inputs = [];
+
+    form.querySelectorAll("input").forEach(input => {
+        const { name, value } = input;
+        inputs.push({ name, value });
+    });
+
+    console.log(inputs);
+
+    alert("Form Submitted Successfully!");
+
+    form.reset();
+    steps.forEach(step => step.classList.remove("active"));
+    steps[0].classList.add("active");
+});
+
+function changeStep(btn) {
+    let index = 0;
+    const active = document.querySelector(".active");
+    index = steps.indexOf(active);
+    steps[index].classList.remove("active");
+
+    if (btn === "next") {
+        index++;
+    }
+    else if (btn === "prev") {
+        index--;
+    }
+    steps[index].classList.add("active");
+}
